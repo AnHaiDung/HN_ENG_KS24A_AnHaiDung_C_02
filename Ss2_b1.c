8c048120b1fcb51cdfca2c4454a3c3e02e56c3e0
AnHaiDung <anhaidung2k6@gmail.com>
2024-11-20 17:48:20 +0700

    b1

---
+++ Ss2_b1.c
@@ -0,0 +1,16 @@
+
+int main() {
+    // Kieu so nguyên: int
+    int age = 18; // Bien kieu int luu tri giá tri nguyen (vi du: tuoi).
+    
+    // Kieu so thuc: float
+    float height = 1.8f; // Bien kieu float luu tri so thuc (ví d?: chieu cao tinh bang met).
+
+    // Kieu so thuc chinh xac cao: double
+    double pi = 3.141592653589793; // Bien kieu double luu tri so thuc voi do chinh xac cao hon float.
+
+    // Kieu ky tu: char
+    char grade = 'A'; // Bien kieu char luu tru mot ký tu don (ví d?: diem xep loai).
+    
+    return 0;
+}
