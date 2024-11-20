1c4c5f38223ded532640e8b3693d9d10e5e9dfb3
AnHaiDung <anhaidung2k6@gmail.com>
2024-11-20 18:27:50 +0700

    b4

---
+++ Ss2_b4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ int main() {
+ 	int edge = 10;
+ 	int perimeter = 4 * edge;
+ 	int area = edge * edge;
+ 	printf("Canh hinh vuong: %d cm\n", edge);
+    printf("Chu vi hình vuông: %d cm\n", perimeter);
+    printf("Dien tich hinh vuong: %d cm^2\n", area);
+    
+    return 0;
+ }
